fb75651f8588dca7b8d9fba5a8d5ded1a7ad73d7
Eddy Nordica <eddy.nordica@outlook.com>
2021-03-28 04:35:37 +0200

    Use isValidCount for counter amount validation

--- src/app/stores/counterAmount.ts
+++ src/app/stores/counterAmount.ts
@@ -1,12 +1,17 @@
 import { createStore } from './createStore';
-import { isInteger } from '../../lib/number';
+import { isValidCount } from '../stores/counter';
 import { DefaultAppSettings } from '../consts/DefaultAppSettings';
+
+const validateCount = (value: number): value is number => {
+  const schema = isValidCount();
+  return schema.isValidSync(value);
+};
 
 const createCounterAmount = () => {
   const { subscribe, set, reset } = createStore('counterAmount', {
     defaultValue: DefaultAppSettings.counterAmount,
     parser: (value: string) => parseInt(value),
-    validator: (value: number) => isInteger(value) && value >= 1,
+    validator: validateCount,
   });
 
   return {
