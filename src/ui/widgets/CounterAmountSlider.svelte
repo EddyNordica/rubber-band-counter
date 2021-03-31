dff133c54e3cdb613fd136b55a817c203efc883d
Eddy Nordica <eddy.nordica@outlook.com>
2021-04-01 00:27:59 +0200

    Added a11y attributes for the slider

--- src/ui/widgets/CounterAmountSlider.svelte
+++ src/ui/widgets/CounterAmountSlider.svelte
@@ -52,6 +52,9 @@
     min="0"
     max="500"
     step="10"
+    aria-valuemin="1"
+    aria-valuemax="500"
+    aria-valuenow={$counterAmount}
     value={$counterAmount}
     on:input={onInput}
   />
